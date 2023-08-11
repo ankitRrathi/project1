95cfab57ec480d2e4e6923b79e59d860e5177813
ankit rathi <ar487084@gmail.com>
2023-08-11 05:28:22 -0700

    1

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -4,13 +4,11 @@ class Homepage extends StatelessWidget {
  
 
 
-     //final int days=30;
-     //final String time ='Holiday';
+   
   @override
   Widget build(BuildContext context) {
-    var emailtext =TextEditingController();
-    var passtext =TextEditingController();
-    
+    var time =DateTime.now();
+
 
 
 
@@ -20,8 +18,25 @@ class Homepage extends StatelessWidget {
         backgroundColor: Color.fromARGB(255, 132, 95, 14),
         foregroundColor: Color.fromARGB(255, 65, 14, 96),
       ),
-      
-    
+      body: Center(
+        child: Container(
+          width: 200, 
+          height: 200,
+          child : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children :[    Text('Current Time : $time',style : TextStyle(fontSize: 21,fontFamily: 'ankit'),),
+            ElevatedButton(onPressed :(){
+               
+            }, child: Text('Click '),)
+            
+
+            ]
+        
+          
+
+          ),
+        ),
+      ),
     );
   }
 }                                                               
