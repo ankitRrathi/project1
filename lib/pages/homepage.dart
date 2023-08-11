2c225a277cabbc6f9c15e40458e4a1070353ae8b
ankit rathi <ar487084@gmail.com>
2023-08-11 04:06:19 -0700

    Day 1

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+class Homepage extends StatelessWidget {
+   const Homepage({super.key});
+ 
+
+
+     //final int days=30;
+     //final String time ='Holiday';
+  @override
+  Widget build(BuildContext context) {
+    var emailtext =TextEditingController();
+    var passtext =TextEditingController();
+    
+
+
+
+    return Scaffold(
+      appBar:  AppBar(
+        title:   Text("ANKIT"),
+        backgroundColor: Color.fromARGB(255, 132, 95, 14),
+        foregroundColor: Color.fromARGB(255, 65, 14, 96),
+      ),
+      
+    
+    );
+  }
+}                                                               
