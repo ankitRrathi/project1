f0ade779ac17313af4b6f982a26934764fbc6322
ankit rathi <ar487084@gmail.com>
2023-08-12 00:34:04 -0700

    day_2

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+
+
 class Homepage extends StatelessWidget {
    const Homepage({super.key});
  
@@ -7,36 +10,19 @@ class Homepage extends StatelessWidget {
    
   @override
   Widget build(BuildContext context) {
-    var time =DateTime.now();
+    
 
 
 
 
     return Scaffold(
       appBar:  AppBar(
-        title:   Text("ANKIT"),
-        backgroundColor: Color.fromARGB(255, 132, 95, 14),
-        foregroundColor: Color.fromARGB(255, 65, 14, 96),
+        title: const   Text("ANKIT"),
+        backgroundColor: const Color.fromARGB(255, 132, 95, 14),
+        foregroundColor: const Color.fromARGB(255, 65, 14, 96),
       ),
-      body: Center(
-        child: Container(
-          width: 200, 
-          height: 200,
-          child : Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children :[    Text('Current Time : $time',style : TextStyle(fontSize: 21,fontFamily: 'ankit'),),
-            ElevatedButton(onPressed :(){
-               
-            }, child: Text('Click '),)
-            
-
-            ]
-        
-          
-
-          ),
-        ),
-      ),
+      
+  
     );
   }
 }                                                               
