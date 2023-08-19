60f9594c597c720cb0736165d9d9087e10b58b6b
ankit rathi <ar487084@gmail.com>
2023-08-19 03:48:26 -0700

    day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:project1/pages/homepage.dart';
 void main()
 {
-  runApp( const MyApp());
-
+  runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
-    return   MaterialApp(
-      home:const Homepage(),
-      themeMode : ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.red),
-  
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
-    
-
- 
-      );
+    return MaterialApp(
+      home : homepage(),
+    )
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,31 +1,12 @@
 import 'package:flutter/material.dart';
-var emailtext =TextEditingController();
-   var passtext =TextEditingController();
 
+class homepage extends StatelessWidget {
+  const homepage({super.key});
 
-
-class Homepage extends StatelessWidget {
-   const Homepage({super.key});
-   
- 
-
-
-   
   @override
   Widget build(BuildContext context) {
-    
-
-
-
     return Scaffold(
-      appBar:  AppBar(
-        title: const   Text("ANKIT"),
-        backgroundColor: const Color.fromARGB(217, 200, 10, 55),
-        foregroundColor: const  Color.fromARGB(183, 223, 8, 177),
-      ),
-       body: const TextField(
-
-       ),
-    );
+      
+    )
   }
-}                                                               
+}
