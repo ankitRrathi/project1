e5f884f4402331e8a45f5a670cb10b34b389ecc1
ankit rathi <ar487084@gmail.com>
2023-08-19 03:48:43 -0700

    day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:project1/pages/homepage.dart';
 void main()
 {
-  runApp(MyApp());
+  runApp( const MyApp());
+
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home : homepage(),
-    )
+    
+    return   MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:const Homepage(),
+      themeMode : ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.red),
+  
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+    
+
+ 
+      );
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class homepage extends StatelessWidget {
-  const homepage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+      class Homepage extends StatefulWidget {
+        const Homepage({super.key});
       
-    )
-  }
-}
+        @override
+        State<Homepage> createState() => _HomepageState();
+      }
+      
+      class _HomepageState extends State<Homepage> {
+        @override
+        Widget build(BuildContext context) {
+          return Scaffold(
+            body : Center 
+          (child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+               const Text('Select Date ',style:TextStyle(fontSize: 31,fontFamily: 'ankit')),
+              ElevatedButton(onPressed: () async 
+                { DateTime?  datepicked = await  showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2021), lastDate: DateTime(2024));
+                  if( datepicked !=null)
+              {
+                print("Time Selected : ${datepicked.day}- ${datepicked.month} - ${datepicked.year}");
+              };
+
+                }
+              ,   child:  const Text('Date'))
+            
+            ],
+          ))
+
+
+
+          );
+          
+
+        }
+      }                                                               
