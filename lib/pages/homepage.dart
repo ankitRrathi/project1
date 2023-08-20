032a03de4bfde91ccf4c903740a209ab5f23b81f
ankit rathi <ar487084@gmail.com>
2023-08-20 02:36:01 -0700

    day 4

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,39 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
-      class Homepage extends StatefulWidget {
-        const Homepage({super.key});
-      
-        @override
-        State<Homepage> createState() => _HomepageState();
-      }
-      
-      class _HomepageState extends State<Homepage> {
-        @override
-        Widget build(BuildContext context) {
-          return Scaffold(
-            body : Center 
-          (child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-               const Text('Select Date ',style:TextStyle(fontSize: 31,fontFamily: 'ankit')),
-              ElevatedButton(onPressed: () async 
-                { DateTime?  datepicked = await  showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2021), lastDate: DateTime(2024));
-                  if( datepicked !=null)
-              {
-                print("Time Selected : ${datepicked.day}- ${datepicked.month} - ${datepicked.year}");
-              };
-
-                }
-              ,   child:  const Text('Date'))
-            
-            ],
-          ))
+//import 'package:intl/intl.dart';
 
 
+class Homepage extends StatelessWidget {
+  const Homepage
+({super.key});
 
-          );
-          
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    appBar:  AppBar(
+        title: const   Text("ANKIT"),
+        backgroundColor: const Color.fromARGB(255, 5, 130, 111),
+        foregroundColor: const Color.fromARGB(255, 223, 94, 8),
+      ),
+    
 
-        }
-      }                                                               
+
+  );
+  }
+}                                                            
