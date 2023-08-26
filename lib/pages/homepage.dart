6bcef6f1e7b44b13b7eda46a736bf6c80910f19c
ankit rathi <ar487084@gmail.com>
2023-08-25 22:18:35 -0700

    Day 5

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -7,9 +7,27 @@ class Homepage extends StatefulWidget {
   State<Homepage> createState() => _HomepageState();
 }
 
-class _HomepageState extends State<Homepage> {
- var iopacity = 1.0;
- bool istrue =true;
+class _HomepageState extends State<Homepage> with SingleTickerProviderStateMixin {
+ late Animation<double> animation;
+ late AnimationController animationController;
+ late Animation coloranimation;
+ @override
+  void initState() {
+
+    
+    super.initState();
+     animationController =  AnimationController(vsync: this,duration:const Duration(seconds: 3));
+    animation = Tween<double>(begin: 0.0, end: 200.0).animate(animationController);
+    coloranimation= ColorTween(begin: Colors.amber,end : Colors.purple).animate(animationController);
+
+   
+    animationController.addListener(() {setState(() {
+      
+    });
+    });
+    animationController.forward();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -19,43 +37,15 @@ class _HomepageState extends State<Homepage> {
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
-      body:Center(
-        child: Column(  mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AnimatedOpacity(
-              opacity :  iopacity, 
-              duration: Duration(seconds: 2),
-              curve: Curves.easeOutQuad,
-              child: Container(
-                width: 300, 
-                height: 100,
-                color: Colors.amber,
-              ),
-      
-              ),
-              ElevatedButton(onPressed: (){
-                setState(() { 
-                  if(istrue)
-                  {
-                    iopacity = 0.0;
-                    istrue = false;
-                  }
-                  else 
-                  {
-                    iopacity =1.0;
-                    istrue =true;
+      body : Center(
+        child: Container(
+          width: animation.value, 
+          height:  animation.value, 
+          color: coloranimation.value,
 
-                  }
-                  
-
-                });
-
-              }, child: Text('Animation'))
-          ],
         ),
-      )
-
- 
+      ),
+     
     );
   }
 }
