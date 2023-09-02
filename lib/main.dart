98434158ad9d45f8b71222f4a923257428ffdcaf
ankit rathi <ar487084@gmail.com>
2023-09-02 08:18:43 -0700

    another boring day

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
     
   return  Scaffold(
     appBar: AppBar(
-      title:const  Text('Animated Opacity'),
+      title:const  Text('AutoComplete '),
       centerTitle: true,
 
     ),
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -3,27 +3,70 @@
 import 'package:flutter/material.dart';
 //import 'package:project1/widgets/rounded_btn.dart';
  class Homepage extends StatefulWidget {
-   const Homepage({super.key});
+   const Homepage({Key? key}) : super(key: key);
 
   @override
   State<Homepage> createState() => _HomepageState();
 }
 
-class _HomepageState extends State<Homepage>with TickerProviderStateMixin {
-  late final AnimationController _animationController =AnimationController(vsync: this,duration: Duration(seconds: 3));
-  late final Animation<double> animation =CurvedAnimation(parent: animation, curve: Curves.elasticOut);
+class _HomepageState extends State<Homepage>{
+  int acceptedData =0;
+
  
+
   @override
    Widget build(BuildContext context) {
-   return Scaffold(
-    body: Center(
-      child: RotationTransition(
-        turns: animation,
-        child: Padding(padding: EdgeInsets.all(8.0),
-        child: FlutterLogo(),
-        ),),
-    ),
-   );
-    
-   }
-     }
+   return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Draggable<int>(
+          // Data is the value this Draggable stores.
+          data: 10,
+          feedback: Container(
+            color: Colors.deepOrange,
+            height: 100,
+            width: 100,
+            child: const Icon(Icons.directions_run),
+          ),
+          childWhenDragging: Container(
+            height: 100.0,
+            width: 100.0,
+            color: Colors.pinkAccent,
+            child: const Center(
+              child: Text('Child When Dragging'),
+            ),
+          ),
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            color: Colors.lightGreenAccent,
+            child: const Center(
+              child: Text('Draggable'),
+            ),
+          ),
+        ),
+        DragTarget<int>(
+          builder: (
+            BuildContext context,
+            List<dynamic> accepted,
+            List<dynamic> rejected,
+          ) {
+            return Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.cyan,
+              child: Center(
+                child: Text('Value is updated to: $acceptedData'),
+              ),
+            );
+          },
+          onAccept: (int data) {
+            setState(() {
+              acceptedData += data;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
