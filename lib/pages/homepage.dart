0a0c8ef69d12fa8ada3b988f4519cc2b015c3c20
ankit rathi <ar487084@gmail.com>
2023-09-17 00:01:09 +0530

    catalog app it is the beginning of the catalog application

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,10 +1,6 @@
 
-// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:project1/widgets/rounded_btn.dart';
-
-
 
 class Homepage extends StatefulWidget {
   const Homepage({
@@ -18,70 +14,12 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage>{
-  bool change =false;
- 
  
   @override
   Widget build(BuildContext context) {
-    
-    return Material(
-      child: Column(
-        children: [
-          Padding(padding: EdgeInsets.all(10)),
-          Container(height: 200,
-          color: Colors.white,
-            child: Image.asset('assets/images/abc.png',fit: BoxFit.cover,)),
-          Text('Welcome',style: TextStyle(fontSize: 30,fontFamily: 'ankit'),),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Column(
-              children: [
-                TextFormField( decoration: InputDecoration(hintText: 'Enter Username',
-                  label: Text('Username')
-                ),
-                ),
-                SizedBox(height: 10,),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Enter Password',
-                    
-                    label: Text('Password')
-                  ),
-                ),
-                Padding(padding: EdgeInsets.symmetric(vertical: 5)),
-              ],
-            ),
-          ),
-          SizedBox(height: 10,),
-          InkWell(
-            onTap: ()
-            {
-              setState(() {
-                change =false;
-
-              });
-              // Navigator.push(context, MaterialPageRoute(builder: (context)=>RoundButton()));
-            },
-            child: AnimatedContainer(
-              duration: Duration(seconds: 1),
-              width: change? 50 : 150, 
-              height: 50, alignment: Alignment.center,
-              child: Text('Login',style: TextStyle(fontSize: 15,fontFamily: 'ankit',color: Colors.white),),
-              decoration: BoxDecoration(
-                color: Colors.deepPurple,
-                borderRadius: BorderRadius.circular(change ? 15 : 20)
-              ),
-            ),
-          )
-          // ElevatedButton(onPressed: (){
-          //   Navigator.push(context, MaterialPageRoute(builder:(context) => RoundButton() ));
-          // }, child: Text('Login'))
-        ],
-      ),
-    
-
-    );
+  return const Scaffold(
+   
+  );
  }
  }
  
